ef930df31b07b9ba905388fdf98818dd9cf1f7a0
Lingqian Meng <lingqian.meng@expleogroup.com>
2023-03-25 14:12:16 +0100

    #39 implicit conversion and the explicit keyword

---
+++ src/39-ImplicitConverion&KeywordExplicit/main39-1.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<string>
+
+/*
+* Note: It is not recommended to use implicit conversion. 
+*/
+
+class Entity 
+{
+private:
+	std::string m_Name;
+	int m_Age;
+public:
+	Entity(const std::string& name)
+		: m_Name(name), m_Age(-1) {}
+	Entity(int age)
+		: m_Name("Unknown"), m_Age(age) {}
+};
+
+void PrintEntity(const Entity& entity)
+{
+	// printing
+}
+
+int main() 
+{
+	/*
+	* normally create an object 
+	*/
+	Entity a = Entity("cherno");
+	Entity b = Entity(22);
+	Entity c("cherno");
+	Entity d(22);
+
+	/*
+	* To create an object with implicit conversion
+	*/
+	Entity e = std::string("cherno"); // cherno is not a string but a char array. 
+	Entity f = 22;
+
+	/*
+	* Passing by the object input into a function normally
+	*/
+	PrintEntity(Entity("Chernot"));
+	PrintEntity(Entity(22));
+
+	/*
+	* Passing by the object input into a function with using implicit conversion
+	*/
+	PrintEntity(22);	// its amazing to me it works now...
+	PrintEntity(std::string("Cherno")); // Implicit Conversion could only do once in cpp. 1. Char array to string 2. string to Entity object.
+	   
+
+	/*
+	* This will not be working.
+	*/
+	Entity e = "cherno";		// will not work. According to the video from cherno it should work but not. 
+	PrintEntity("Chernot"); // will not work. Because conversions must be happend twice.
+
+	std::cin.get();
+}
