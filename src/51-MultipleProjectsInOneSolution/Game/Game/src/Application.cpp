68f31848df1c6f57b79d7c963abdd6fd570ccf6e
lingqianmeng <binyi1114@gmail.com>
2023-04-11 18:00:12 +0200

    #51 Multiple projects in the same solution

---
+++ src/51-MultipleProjectsInOneSolution/Game/Game/src/Application.cpp
@@ -0,0 +1,13 @@
+#include "Engine.h"
+#include<iostream>
+
+/*
+* The project Game will reference the project Engine in the same solution by using static linking
+*/
+
+int main() 
+{
+	engine::PrintMessage();
+	std::cin.get();
+
+}
